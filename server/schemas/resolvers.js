6bf3957938096e566bdd8fd0d419581c96fde0d2
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-22 11:14:58 -0400

    we all git ignored up, fixed server package.json

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,6 @@
-const { Thought } = require('../models');
-
 const resolvers = {
   Query: {
-    question: async () => [
-        { 
-            question:"who is the better team?",
-            options:["group 1", "group 2", "group 3"]
-    },
-]
 
+  }
+};
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,5 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
-    type poll {
-        _id: ID
-        question: String
-        options: Array
-
-    }
-
-    type Query {
-        poll: [poll]
-    }
-`;
+const typeDefs = gql``;
 
 module.exports = typeDefs;
