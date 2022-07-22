880db57481d85e86122c226865e1c60749c49f8f
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-22 11:15:26 -0400

    deleted package.json

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,11 @@
 const resolvers = {
   Query: {
-
+    question: async () => [
+      { 
+        question:"who is the better team?",
+        options:["group 1", "group 2", "group 3"]
+      },
+    ]
   }
 };
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,16 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type poll {
+        _id: ID
+        question: String
+        options: [String]
+
+    }
+
+    type Query {
+        poll: [poll]
+    }
+`;
 
 module.exports = typeDefs;
