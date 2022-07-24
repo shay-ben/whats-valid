f20941450ba6a8a3768d71f21db8e378fcaa2ca2
Your Name <you@example.com>
2022-07-24 12:09:32 -0400

    installed vite globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import image from "./images/BACKGROUND.png"; 
+
+function Component() {
+  return (
+    <div style={{ backgroundImage:`url(${image})` }}>
+      Hello World
+    </div>
+  );
+}
+
+export { Component };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
