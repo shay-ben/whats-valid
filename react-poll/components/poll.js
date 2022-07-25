df4b001626afa13f50e3873df1f81ee4beb8c91e
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-25 14:15:24 -0400

    display vote count

--- react-poll/components/poll.js
+++ react-poll/components/poll.js
@@ -49,7 +49,15 @@ if (voteData) {
         </button>          
       </li>
     );
-  });
-}
+});
+return (
+    <div className="poll">
+      <h1>Which option do you like the best?</h1>
+      <ul className={voted ? "results" : "options"}>
+        {pollOptions}
+      </ul>
+      <p>Total Votes: {totalVotes}</p>
+    </div>
+  );
 }
 export default Poll;
