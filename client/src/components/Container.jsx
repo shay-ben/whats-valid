4a08492e223c79b2184793cf26262d0acb24e146
Roy Kathurima <kathurimaroy@gmail.com>
2022-07-25 19:13:43 +0300

    refactored the container component into a shareable component

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background-color: #ccc;
+  display: grid;
+  padding: 20px;
+  place-items: flex-start;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 20px 20px 20px;
+  row-gap: 10px;
+  text-align: left;
+  box-shadow: 5px 10px 18px #888888;
+  border-radius: 5px;
+  label {
+    margin-right: 10px;
+    grid-column-start: 1;
+  }
+  input {
+    height: fit-content;
+    width: 300px;
+  }
+  input[type='submit'] {
+    grid-column-start: 2;
+    background-color: green;
+    padding: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    width: fit-content;
+  }
+`;
+
+export {
+  Container
+}
