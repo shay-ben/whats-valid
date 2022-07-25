711c99ae159d303b65e3bc49b8c77193c214a162
Roy Kathurima <kathurimaroy@gmail.com>
2022-07-25 18:54:27 +0300

    added styling to the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,40 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background-color: #ccc;
+  display: grid;
+  padding: 20px;
+  place-items: flex-start;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 20px 20px 20px;
+  row-gap: 10px;
+  text-align: left;
+  box-shadow: 5px 10px 18px #888888;
+  border-radius: 5px;
+  label {
+    margin-right: 10px;
+    grid-column-start: 1;
+  }
+  input[type='submit'] {
+    grid-column-start: 2;
+    background-color: green;
+    padding: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  input {
+    height: fit-content;
+  }
+`;
+
 export const Login = () => {
   return (
-    <span>This the login Page</span>
+    <Container>
+      <label htmlFor='email'>Email:</label>
+      <input id='email' type='email' />
+      <label htmlFor='password'>Password:</label>
+      <input id='password' type='password' />
+      <input type='submit'/>
+    </Container>
   )
 }
