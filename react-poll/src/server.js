2796e5a047eb855c8811ab672a68f88a116ce127
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-25 13:44:10 -0400

    add post function

--- react-poll/src/server.js
+++ react-poll/src/server.js
@@ -11,5 +11,17 @@ const pollData = require("./data.json");
 app.get("/poll", function (req, res) {
   res.send(pollData);
 });
+app.post("/poll", function (req, res) {  
+    if (req.body) {
+      fs.writeFileSync("data.json", JSON.stringify(req.body));
+      res.send({
+        message: "Data Saved",
+      });
+    } else {
+      res.status(400).send({
+        message: "Error No Data",
+      });
+    }
+  });
 
 app.listen(5000, () => console.log("Server Running..."));
