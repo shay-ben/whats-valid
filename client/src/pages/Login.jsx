f6992e9f8d82c9fd8c8a94c95c0a8cce9a1b9def
Roy Kathurima <kathurimaroy@gmail.com>
2022-07-25 20:13:54 +0300

    added basic validation to the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,41 @@
-import { Container } from '../components/styled-components';
+import { Container, ErrorText } from '../components/styled-components';
 
 export const Login = () => {
+  const [errors, setErrors] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const updateData = mode => event => {
+    if (errors) {
+      setErrors('')
+    }
+    switch(mode) {
+      case 'username':
+        setUsername(event.target.value);
+        break;
+      case 'password':
+        setPassword(event.target.value);
+        break;
+      default:
+        console.error('something went wrong');
+    }
+  }
+
+  const onSubmit = () => {
+    // Validate on submit
+    if (!username || !password) {
+      setErrorState('All fields are required');
+      return;
+    }
+  }
   return (
     <Container>
-      <label htmlFor='email'>Email:</label>
-      <input id='email' type='email' />
+      <label htmlFor='username'>User Name:</label>
+      <input value={username} onChange={updateData('username')} id='username' type='text' required/>
       <label htmlFor='password'>Password:</label>
-      <input id='password' type='password' />
-      <input type='submit'/>
+      <input value={password} onChange={updateData('password')} id='password' type='password' required/>
+      { errors ? <ErrorText>{errors}</ErrorText> : null}
+      <input onClick={onSubmit} type='submit'/>
     </Container>
   )
 }
