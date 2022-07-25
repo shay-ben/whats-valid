fa24af92b6c9e2449b8e060a9317f7c7f8b2eda5
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-25 13:59:33 -0400

    attempting to use fetch API to get response

--- react-poll/components/poll.js
+++ react-poll/components/poll.js
@@ -6,4 +6,19 @@ const [voteData, setVoteData] = useState();
 const [totalVotes, setTotalVotes] = useState(0);
 const [voted, setVoted] = useState(false);
 }
+
+const url = "http://localhost:5000/poll";
+useEffect(() => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      setVoteData(data);
+      let sum = 0;
+      data.forEach(function (obj) {
+        sum += obj.votes;
+      });
+      setTotalVotes(sum);
+    });
+}, []);
+}
 export default Poll;
