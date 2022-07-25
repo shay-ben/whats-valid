111ffcaeed3b4ce16b497e6f82c87cdb0e76e0e8
Roy Kathurima <kathurimaroy@gmail.com>
2022-07-25 22:55:37 +0300

    added login logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+import { LOGIN_USER } from '../utils/mutations';
 import { Container, ErrorText } from '../components/styled-components';
 
 export const Login = () => {
   const [errors, setErrors] = useState('');
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [mutate, result] = useMutation(LOGIN_USER, { errorPolicy: true });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (result.called && result.data?.login?.token && !result.loading) {
+      localStorage.setItem('id_token', result.data?.login?.token);
+      navigate('/', { replace: true });
+    }
+  }, [result.called, result.loading, result.data])
 
   const updateData = mode => event => {
     if (errors) {
       setErrors('')
     }
     switch(mode) {
-      case 'username':
-        setUsername(event.target.value);
+      case 'email':
+        setEmail(event.target.value);
         break;
       case 'password':
         setPassword(event.target.value);
@@ -24,15 +36,16 @@ export const Login = () => {
 
   const onSubmit = () => {
     // Validate on submit
-    if (!username || !password) {
+    if (!email || !password) {
       setErrorState('All fields are required');
       return;
     }
+    mutate({ variables: { email, password } });
   }
   return (
     <Container>
-      <label htmlFor='username'>User Name:</label>
-      <input value={username} onChange={updateData('username')} id='username' type='text' required/>
+      <label htmlFor='email'>User Name:</label>
+      <input value={email} onChange={updateData('email')} id='email' type='email' required/>
       <label htmlFor='password'>Password:</label>
       <input value={password} onChange={updateData('password')} id='password' type='password' required/>
       { errors ? <ErrorText>{errors}</ErrorText> : null}
