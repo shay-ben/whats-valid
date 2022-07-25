f2bd3179ade572748cb888e0fa4b966bd70a9556
Sukhjit Singh <sukhjitsingh@Sukhjits-MacBook-Air.local>
2022-07-25 12:32:12 -0400

    components

---
+++ client/src/components/QuestionPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Question from './Question'
+import { Link } from 'react-router-dom'
+
+class QuestionPage extends Component {
+  render() {
+    const {id} = this.props
+    return (
+      <div> 
+      	<Question id={id} />
+		    <button>
+        	<Link to='/'> Back </Link>
+		    </button>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ authedUser, questions, users }, props) {
+  const { id } = props.match.params
+  return { id }
+}
+export default connect(mapStateToProps)(QuestionPage)
