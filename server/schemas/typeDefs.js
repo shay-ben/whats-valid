b99f7edffd7caa2f91856bcd5fd486c385d1bbf4
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-25 13:13:01 -0400

    updating the typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,7 @@ type User {
   _id: ID
   username: String
   email: String
+  pollsVoted: [ID]
 }
 type Option {
   _id: ID
@@ -22,18 +23,20 @@ type Auth {
 }
 type Query {
   users: [User]
-  user(username: String!): User
+  user(id: ID!): User
   me: User
   polls: [Poll]
   poll(id: ID!): Poll
+  hasVoted(pollID: ID!, userID: ID!): Boolean
 }
 type Mutation {
   addUser(username: String!, email: String!, password: String!): Auth
   login(email: String!, password: String!): Auth
+  deleteUser(id: ID!): String
   createPoll(name: String, question: String!, optionsArr: [String]): Poll
   deletePoll(id: ID!): String
   deleteAllPolls: String
-  vote(pollID: ID!, optionID: ID!): Poll
+  vote(pollID: ID!, optionID: ID!, userID: ID!): Poll
 }
 `;
 module.exports = typeDefs;
