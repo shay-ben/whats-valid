2db4ff7d15f17141cbf05b17eef9c0e5a637199e
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-25 13:51:24 -0400

    add state variables

--- react-poll/components/poll.js
+++ react-poll/components/poll.js
@@ -2,5 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./Poll.css";
 
 function Poll() {  
+const [voteData, setVoteData] = useState();
+const [totalVotes, setTotalVotes] = useState(0);
+const [voted, setVoted] = useState(false);
 }
 export default Poll;
