89d93df4fa14a9dd5845a1a8f054595b004ea33d
Your Name <you@example.com>
2022-07-27 03:23:32 -0400

    more adjusting

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -4,7 +4,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-
 // import { useState, useEffect } from 'react';
 // import { useMutation } from '@apollo/client';
 // import { useNavigate } from 'react-router-dom';
